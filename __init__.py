2dc9ad2777840103addde4a258e27ee507b2e504
QuasiPlanets <quasimarvin@gmail.com>
2025-07-31 18:44:02 -0400

    feat: initial XTTS custom node implementation with complete TTS library

---
+++ __init__.py
@@ -0,0 +1,69 @@
+import site
+import os,sys
+import logging
+from server import PromptServer
+
+now_dir = os.path.dirname(os.path.abspath(__file__))
+site_packages_roots = []
+for path in site.getsitepackages():
+    if "packages" in path:
+        site_packages_roots.append(path)
+if(site_packages_roots==[]):site_packages_roots=["%s/runtime/Lib/site-packages" % now_dir]
+#os.environ["OPENBLAS_NUM_THREADS"] = "4"
+
+# Try to add XTTS path to site-packages, but don't fail if we can't
+pth_file_created = False
+for site_packages_root in site_packages_roots:
+    if os.path.exists(site_packages_root):
+        try:
+            with open("%s/XTTS.pth" % (site_packages_root), "w") as f:
+                f.write(
+                    "%s\n%s/TTS\n"
+                    % (now_dir,now_dir)
+                )
+            pth_file_created = True
+            break
+        except PermissionError:
+            print(f"Warning: Could not write XTTS.pth to {site_packages_root} - continuing without it")
+            continue
+
+if pth_file_created:
+    print("!!!XTTS path was added to " + "%s/XTTS.pth" % (site_packages_root) 
+    + "\n if meet `No module` error,try `python main.py` again")
+else:
+    print("Note: XTTS.pth file could not be created due to permissions, but the custom node should still work")
+
+
+WEB_DIRECTORY = "./web"
+from .nodes import LoadSRT,LoadAudioPath, PreViewAudio,XTTS_INFER, XTTS_INFER_SRT
+
+# Set the web directory, any .js file in that directory will be loaded by the frontend as a frontend extension
+# WEB_DIRECTORY = "./somejs"
+
+# A dictionary that contains all nodes you want to export with their names
+# NOTE: names should be globally unique
+NODE_CLASS_MAPPINGS = {
+    "LoadAudioPath": LoadAudioPath,
+    "PreViewAudio": PreViewAudio,
+    "LoadSRT": LoadSRT,
+    "XTTS_INFER": XTTS_INFER,
+    "XTTS_INFER_SRT": XTTS_INFER_SRT
+}
+
+# A dictionary that contains the friendly/humanly readable titles for the nodes
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "LoadAudioPath": "LoadAudioPath",
+    "PreViewAudio": "PreView Audio",
+    "LoadSRT": "SRT FILE Loader",
+    "XTTS_INFER": "XTTS Inference",
+    "XTTS_INFER_SRT": "XTTS Inference with srt"
+}
+
+@PromptServer.instance.routes.get("/xtts/reboot")
+def restart(self):
+    try:
+        sys.stdout.close_log()
+    except Exception as e:
+        pass
+
+    return os.execv(sys.executable, [sys.executable] + sys.argv)
